d647e1cd575bde5bf34450de6bf3d4b12cce2f5e
Hari2023 <haribaskarg93@gmail.com>
2023-03-22 12:57:19 +0530

    push

---
+++ Test 1/src/com/loop/prac/Class_One.java
@@ -0,0 +1,21 @@
+package com.loop.prac;
+
+public class Class_One {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Increment");
+		for (int i = 1; i<=5; i++) {
+			System.out.println(i);
+					
+		}
+		
+		System.out.println("Decrement");
+		for(int i =5; i>=1; i--) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
