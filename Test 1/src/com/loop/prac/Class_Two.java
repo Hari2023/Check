c2ad2cbce2fa652314b7084c15de6b7f50eb925a
Hari2023 <haribaskarg93@gmail.com>
2023-03-22 13:03:50 +0530

    First commit

---
+++ Test 1/src/com/loop/prac/Class_Two.java
@@ -0,0 +1,22 @@
+package com.loop.prac;
+
+public class Class_Two {
+	
+	// while loop
+	public static void main(String[] args) {
+		
+		int a = 1;
+		
+     while (a<=10) {
+		System.out.println(a);
+		a++;
+	}
+		
+		
+		
+		/*do {
+			System.out.println("true");
+		} while (a>=10);*/
+	}
+
+}
